d488585381cb9198353830bc2add66d698bc8a75
Blagovest <blagovest.kolev@gmail.com>
2022-10-19 14:43:10 +0300

    Create Chat hub in asp.Net core.

---
+++ ChatApp/Hubs/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ChatApp.Hubs
+{
+    public class ChatHub : Hub
+    {
+        private readonly string bot;
+        public ChatHub()
+        {
+            this.bot = "Chat-Bot";
+        }
+        public async Task JoinRoom(UserConnection userConnection)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, userConnection.Room);
+            await Clients.Group(userConnection.Room).SendAsync("ReceiveMessage", this.bot, $"{userConnection.User}has joined {userConnection.Room}.");
+        }
+    }
+}
--- ChatApp/Program.cs
+++ ChatApp/Program.cs
@@ -1,6 +1,15 @@
+using ChatApp.Hubs;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSignalR();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapHub<ChatHub>("/chat");
+});
 
 app.Run();
