552aa8786bfeefd6cb1708a3a31519df94bb3e7e
Blagovest <blagovest.kolev@gmail.com>
2022-10-20 15:45:18 +0300

    Made a smooth slide for new messages when they appear.

--- chat-app-frontend/src/components/MessageContainer.js
+++ chat-app-frontend/src/components/MessageContainer.js
@@ -1,7 +1,21 @@
+import { useEffect, useRef } from 'react';
 
 function MessageContainer({ messages }) {
+
+    const messageRef = useRef();
+
+    useEffect(() => {
+        if (messageRef && messageRef.current) {
+            const { scrollHeight, clientHeight } = messageRef.current;
+            messageRef.current.scrollTo({
+                left: 0, top: scrollHeight - clientHeight,
+                behavior: 'smooth'
+            });
+        }
+    }, [messages])
+
     return (
-        <div className="message-container">
+        <div ref={messageRef} className="message-container">
             {messages.map((msg, index) =>
                 <div key={index} className='user-message'>
                     <div className="message bg-primary">{msg.message}</div>
